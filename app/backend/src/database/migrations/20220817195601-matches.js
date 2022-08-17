701d91ca8ac429e4c4e048658977a50b86c557c5
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-17 17:19:24 -0300

    ci: :card_file_box: Create matches migration.

---
+++ app/backend/src/database/migrations/20220817195601-matches.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: { 
+        type: Sequelize.INTEGER,
+        allowNull: false, 
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      homeTeam: {
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        references: {
+          model: 'teams',
+          key:'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        field: 'home_team',
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        // allowNull: false, 
+        field: 'home_team_goals',
+        defaultValue: 0,
+      },
+      awayTeam: {
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        references: {
+          model: 'teams',
+          key:'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        field: 'away_team',
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        // allowNull: false, 
+        field: 'home_team_goals',
+        defaultValue: 0,
+      },
+      inprogress: {
+        type: Sequelize.BOOLEAN,
+        // allowNull: false,
+        defaultValue: false,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
