5784428759554fe9fd1523814c7d251470bed923
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-18 17:27:25 -0300

    fix: Add Joi error identification to the error midleware.

--- app/backend/src/middleware/error.middleware.ts
+++ app/backend/src/middleware/error.middleware.ts
@@ -2,6 +2,11 @@ import { ErrorRequestHandler } from 'express';
 
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   const { message, statusCode } = err;
+  if (err.isJoi) {
+    return res.status(400).json({
+      message: err.details[0].message,
+    });
+  }
   if (message && statusCode) {
     res.status(statusCode).json({ message });
   }
