538f0421fae2c44a62a8d72add90c6db23b01508
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-18 17:21:19 -0300

    fix: Remove atribute name of newError class.

--- app/backend/src/helpers/NewError.ts
+++ app/backend/src/helpers/NewError.ts
@@ -1,10 +1,8 @@
 export default class NewError extends Error {
-  name: string;
   statusCode: number | undefined;
 
-  constructor(message:string, name:string, statusCode:number) {
+  constructor(message:string, statusCode:number) {
     super(message);
-    this.name = name;
     this.statusCode = statusCode;
   }
 }
