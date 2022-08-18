ecc026ee66f7c97ead1ba81f8ba67ad0f1adf271
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-18 20:46:11 -0300

    feat: Create UsersService.
    It provides a list and findOneByEmail methods

---
+++ app/backend/src/services/users.service.ts
@@ -0,0 +1,18 @@
+import Users from '../database/models/users';
+
+export interface IUserService {
+  list(): Promise<Users[]>,
+  findOneByEmail(email: string): Promise<Users | null>,
+}
+
+export class UsersService implements IUserService {
+  list = async (): Promise<Users[]> => {
+    const users: Users[] = await Users.findAll();
+    return users;
+  };
+
+  findOneByEmail = async (email: string): Promise<Users | null> => {
+    const user: Users | null = await Users.findOne({ where: { email } });
+    return user;
+  };
+}
