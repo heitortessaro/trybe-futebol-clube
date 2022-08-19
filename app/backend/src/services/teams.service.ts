f2df6580b139f3430c7370767062e6dfa745bb0b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-19 19:46:09 -0300

    feat: :card_file_box: Create TeamsService.
    It provides the method list and findById.

---
+++ app/backend/src/services/teams.service.ts
@@ -0,0 +1,18 @@
+import Teams from '../database/models/teams';
+
+export interface ITeamsService {
+  list(): Promise<Teams[]>,
+  findById(id:number): Promise<Teams | null>,
+}
+
+export class TeamsService implements ITeamsService {
+  list = async (): Promise<Teams[]> => {
+    const teams: Teams[] = await Teams.findAll();
+    return teams;
+  };
+
+  findById = async (id:number): Promise<Teams | null> => {
+    const team: Teams | null = await Teams.findByPk(id);
+    return team;
+  };
+}
