8ef3ed9ec9d7facdb8c528d719b1a37c5e65d2b0
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-20 09:12:04 -0300

    fix: :bug: await was missing in async calls.

--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -7,13 +7,13 @@ export default class TeamsController {
   constructor(private teamsService:ITeamsService) {}
 
   listTeams = async (_req: Request, res: Response): Promise<void> => {
-    const teams = this.teamsService.list();
+    const teams = await this.teamsService.list();
     res.status(StatusCodes.OK).json(teams);
   };
 
   getTeamWithId = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
-    const team = this.teamsService.findById(id);
+    const team = await this.teamsService.findById(id);
     res.status(StatusCodes.OK).json(team);
   };
 }
