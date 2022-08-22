578001080ab60116e9b93b631e6bc2e953e54eba
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-22 15:00:11 -0300

    feat: :card_file_box: Create an association between teams and matches models.

--- app/backend/src/database/models/matches.ts
+++ app/backend/src/database/models/matches.ts
@@ -1,6 +1,8 @@
 import { INTEGER, Model, BOOLEAN } from 'sequelize';
 import db from '.';
 
+import Teams from './teams';
+
 class Matches extends Model {
   id!: number;
   homeTeam!: string;
@@ -39,6 +41,23 @@ Matches.init({
   modelName: 'matches',
   underscored: true,
   timestamps: false,
+});
+
+// Matches.belongsTo(Teams, { foreignKey: 'homeTeam' });
+// Matches.belongsTo(Teams, { foreignKey: 'awayTeam' });
+
+// Teams.hasMany(Matches, { foreignKey: 'homeTeam' });
+// Teams.hasMany(Matches, { foreignKey: 'awayTeam' });
+
+Teams.hasMany(Matches, {
+  sourceKey: 'id',
+  foreignKey: 'homeTeam',
+  as: 'homeTeam',
+});
+Teams.hasMany(Matches, {
+  sourceKey: 'id',
+  foreignKey: 'awayTeam',
+  as: 'awayTeam',
 });
 
 export default Matches;
