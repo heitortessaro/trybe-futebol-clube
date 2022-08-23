8fb27e1c9a8ae199ea47f3c3e9478bd35c527c71
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-23 18:18:12 -0300

    feat: Update matchesMiddleware methods and create CheckGameInProgress.
    CheckgameInProgress verify if the match is inprogress.  Now, the checkMatchExist add the inProgress information to the req.body if the match exists. Similarly, now the updateMatchInfoExist add the newScore info to the body if the info pass validation.

--- app/backend/src/middleware/matches.middleware.ts
+++ app/backend/src/middleware/matches.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import Joi = require('joi');
+import IMatchScore from '../interfaces/IMatchScore';
 import { ITeamsService } from '../services/teams.service';
 import NewError from '../helpers/NewError';
 import IMatchInProgress from '../interfaces/IMatchInProgress';
@@ -32,20 +33,32 @@ export default class MatchesMiddlewares {
     const { id } = req.params;
     const match = await this.matchesService.findMatchById(parseInt(id, 10));
     if (!match) throw new NewError('There is no match with such id!', StatusCodes.NOT_FOUND);
+    const { inProgress } = match;
+    req.body.inProgress = inProgress;
     req.body.id = parseInt(id, 10);
     next();
   };
 
   updateMatchInfoExist = async (req: Request, _res: Response, next: NextFunction) => {
-    const matchInfo = req.body;
+    const { homeTeamGoals, awayTeamGoals } = req.body;
     const schema = Joi.object({
       homeTeamGoals: Joi.number().integer().required(),
       awayTeamGoals: Joi.number().integer().required(),
     });
     try {
-      await schema.validateAsync(matchInfo);
-    } catch (_error) {
+      await schema.validateAsync({ homeTeamGoals, awayTeamGoals });
+    } catch (error) {
+      console.log(error);
       throw new NewError('All fields must be filled', StatusCodes.BAD_REQUEST);
+    }
+    req.body.newScore = { homeTeamGoals, awayTeamGoals } as IMatchScore;
+    next();
+  };
+
+  checkGameIsInProgress = (req: Request, _res: Response, next: NextFunction) => {
+    const { inProgress } = req.body;
+    if (!inProgress) {
+      throw new NewError('The match has already been finished', StatusCodes.BAD_REQUEST);
     }
     next();
   };
