1cb76c78c6b0e05c82127c922efb51ec4f255aca
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-23 15:46:32 -0300

    feat: Create addMatchInProgress method to the MatchesService.
    It return an object of type IMatch. A bug in the listMatchesInProgress was also fixed.

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,10 +1,15 @@
 // import Teams from 'src/database/models/teams';
+import { StatusCodes } from 'http-status-codes';
+import IMatchInProgress from '../interfaces/IMatchInProgress';
+import NewError from '../helpers/NewError';
 import Teams from '../database/models/teams';
 import Matches from '../database/models/matches';
+import IMatch from '../interfaces/IMatch';
 
 export interface IMatchesService {
   listMatches(): Promise<Matches[]>,
   listMatchesInProgress(inProgress:boolean): Promise<Matches[]>,
+  addMatchInProgress(matchInfo:IMatchInProgress): Promise<IMatch>,
 }
 
 export default class MatchesService implements IMatchesService {
@@ -29,12 +34,30 @@ export default class MatchesService implements IMatchesService {
 
   listMatchesInProgress = async (inProgress:boolean): Promise<Matches[]> => {
     const matches = await Matches.findAll({
-      where: { title: { inProgress } },
+      where: { inProgress },
       include: [
         { model: Teams, as: 'teamHome', attributes: { include: ['teamName'] } },
         { model: Teams, as: 'teamAway', attributes: { include: ['teamName'] } },
       ],
     });
     return matches;
+  };
+
+  addMatchInProgress = async (matchInfo:IMatchInProgress) : Promise<IMatch> => {
+    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = matchInfo;
+    const inProgress = true;
+    const result = await Matches
+      .create({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress });
+    if (result) {
+      return {
+        id: result.id,
+        homeTeam,
+        awayTeam,
+        homeTeamGoals,
+        awayTeamGoals,
+        inProgress,
+      } as IMatch;
+    }
+    throw new NewError('Error adding data to the DB', StatusCodes.INTERNAL_SERVER_ERROR);
   };
 }
