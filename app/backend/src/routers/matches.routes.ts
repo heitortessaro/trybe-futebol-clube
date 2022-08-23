82793093f8ae5e9f209b4b6be845ece2d9ee156a
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-23 16:46:25 -0300

    feat: Add validation middlewares to the post /matches route.
    Ensure teams are not equal and exist. In addition, just request with valid token can add matches.

--- app/backend/src/routers/matches.routes.ts
+++ app/backend/src/routers/matches.routes.ts
@@ -1,19 +1,25 @@
 import { Router } from 'express';
+import MatchesMiddlewares from '../middleware/matches.middleware';
 import MatchesController from '../controllers/matches.controllers';
 import MatchesService from '../services/matches.service';
+import { TeamsService } from '../services/teams.service';
+import UsersMiddleware from '../middleware/users.middleware';
+import { UsersService } from '../services/users.service';
 
 const router = Router();
 
+const usersService = new UsersService();
+const teamsService = new TeamsService();
 const matchesService = new MatchesService();
 const matchesController = new MatchesController(matchesService);
-
-// router.get(
-//   '/matches?inProgress',
-//   matchesController.listMatchesInProgress,
-// );
+const matchesMiddleware = new MatchesMiddlewares(matchesService, teamsService);
+const usersMiddleware = new UsersMiddleware(usersService);
 
 router.post(
   '/matches',
+  matchesMiddleware.checkTeamsNotEqual,
+  matchesMiddleware.checkTeamsExist,
+  usersMiddleware.validateAuthorizationToken,
   matchesController.addMatchInProgress,
 );
 
