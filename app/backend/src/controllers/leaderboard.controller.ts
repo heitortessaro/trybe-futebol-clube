1d9af1dabcd9b73c0256e07a4e339281df02e3f8
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-24 15:59:51 -0300

    feat: :construction: Add getLeaderboard to the LeaderboardController.
    The getLeaderboardHome method is not working yet.

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -4,18 +4,36 @@ import { ILeaderboardService } from '../services/leaderboard/leaderboard.service
 import { IMatchesService } from '../services/databaseInteraction/matches.service';
 // import Matches from '../database/models/matches';
 import { IMatchComplete } from '../interfaces/IMatchComplete';
+import { ITeamsService } from '../services/databaseInteraction/teams.service';
 
 export default class LeaderboardController {
   constructor(
     private matchesService:IMatchesService,
     private leaderboardService:ILeaderboardService,
+    private teamsService:ITeamsService,
   ) {}
 
   getLeaderboardHome = async (_req: Request, res: Response): Promise<void> => {
     const inProgress = false;
     const endedMatches = await this.matchesService.listMatchesInProgress(inProgress);
-    const leaderboard = await this
-      .leaderboardService.returnLeaderBoardHome(endedMatches as unknown as IMatchComplete);
+    const teams = await this.teamsService.list();
+    const leaderboard = this
+      .leaderboardService.LeaderboardHome(
+        endedMatches as unknown as IMatchComplete[],
+        teams,
+      );
+    res.status(StatusCodes.OK).json(leaderboard);
+  };
+
+  getLeaderboard = async (_req: Request, res: Response): Promise<void> => {
+    const inProgress = false;
+    const endedMatches = await this.matchesService.listMatchesInProgress(inProgress);
+    const teams = await this.teamsService.list();
+    const leaderboard = this
+      .leaderboardService.Leaderboard(
+        endedMatches as unknown as IMatchComplete[],
+        teams,
+      );
     res.status(StatusCodes.OK).json(leaderboard);
   };
 }
