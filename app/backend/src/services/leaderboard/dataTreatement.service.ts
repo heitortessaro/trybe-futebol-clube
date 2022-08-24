26efab1cb108df08587ebad7661c149549240fd4
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-24 19:48:07 -0300

    feat: :sparkles: Can select away, home, or both data from DTS
    Functions checkDrawn, checkVictories, and countGoals now offer the option to save just the information of home games, away games or both to build the leaderboard. To fulfill eslint requirements, now the checkVictories uses two auxiliary functions to increment victories and losses games. The sorting function have also been refactored to be more clear using auxiliary functions.

--- app/backend/src/services/leaderboard/dataTreatement.service.ts
+++ app/backend/src/services/leaderboard/dataTreatement.service.ts
@@ -2,32 +2,47 @@ import { IMatchComplete } from '../../interfaces/IMatchComplete';
 import { ILeaderboard } from '../../interfaces/ILeaderboard';
 
 export interface ILeaderboardDTS {
-  checkDrawn(matches:IMatchComplete[], leadeboard:ILeaderboard[]):ILeaderboard[],
-  checkVictories(matches:IMatchComplete[], leadeboard:ILeaderboard[]):ILeaderboard[],
-  countGoals(matches:IMatchComplete[], leadeboard:ILeaderboard[]):ILeaderboard[],
-  calcGoalBalance(leaderboard:ILeaderboard[]): ILeaderboard[],
-  calcEfficiency(leaderboard:ILeaderboard[]): ILeaderboard[],
-  calcTotalGames(leaderboard:ILeaderboard[]): ILeaderboard[],
-  calcTotalPoints(leaderboard:ILeaderboard[]): ILeaderboard[],
-  sortClassification(leaderboard:ILeaderboard[]): ILeaderboard[],
+  checkDrawn(
+    matches: IMatchComplete[],
+    leadeboard: ILeaderboard[],
+    includAwayInfo:boolean,
+    includeHomeInfo:boolean,
+  ): ILeaderboard[],
+  checkVictories(
+    matches: IMatchComplete[],
+    leadeboard: ILeaderboard[],
+    includAwayInfo:boolean,
+    includeHomeInfo:boolean,
+  ): ILeaderboard[],
+  countGoals(
+    matches: IMatchComplete[],
+    leadeboard: ILeaderboard[],
+    includAwayInfo:boolean,
+    includeHomeInfo:boolean,
+  ): ILeaderboard[],
+  calcGoalBalance(leaderboard: ILeaderboard[]): ILeaderboard[],
+  calcEfficiency(leaderboard: ILeaderboard[]): ILeaderboard[],
+  calcTotalGames(leaderboard: ILeaderboard[]): ILeaderboard[],
+  calcTotalPoints(leaderboard: ILeaderboard[]): ILeaderboard[],
+  sortClassification(leaderboard: ILeaderboard[]): ILeaderboard[],
 }
 
 export default class LeaderboardDTService implements ILeaderboardDTS {
-  private processVictory = (leadeboard: ILeaderboard[], teamName: string):ILeaderboard[] => {
+  private processVictory = (leadeboard: ILeaderboard[], teamName: string): ILeaderboard[] => {
     const newLeaderboard = leadeboard;
     const teamIndex = leadeboard.findIndex((team) => team.name === teamName);
     newLeaderboard[teamIndex].totalVictories += 1;
     return newLeaderboard;
   };
 
-  private processDraw = (leadeboard: ILeaderboard[], teamName: string):ILeaderboard[] => {
+  private processDraw = (leadeboard: ILeaderboard[], teamName: string): ILeaderboard[] => {
     const newLeaderboard = leadeboard;
     const teamIndex = leadeboard.findIndex((team) => team.name === teamName);
     newLeaderboard[teamIndex].totalDraws += 1;
     return newLeaderboard;
   };
 
-  private processLoss = (leadeboard: ILeaderboard[], teamName: string):ILeaderboard[] => {
+  private processLoss = (leadeboard: ILeaderboard[], teamName: string): ILeaderboard[] => {
     const newLeaderboard = leadeboard;
     const teamIndex = leadeboard.findIndex((team) => team.name === teamName);
     newLeaderboard[teamIndex].totalLosses += 1;
@@ -37,9 +52,9 @@ export default class LeaderboardDTService implements ILeaderboardDTS {
   private processGoals = (
     leadeboard: ILeaderboard[],
     teamName: string,
-    goalsFavor:number,
-    goalsOwn:number,
-  ):ILeaderboard[] => {
+    goalsFavor: number,
+    goalsOwn: number,
+  ): ILeaderboard[] => {
     const newLeaderboard = leadeboard;
     const teamIndex = leadeboard.findIndex((team) => team.name === teamName);
     newLeaderboard[teamIndex].goalsFavor += goalsFavor;
@@ -47,52 +62,118 @@ export default class LeaderboardDTService implements ILeaderboardDTS {
     return newLeaderboard;
   };
 
-  checkDrawn = (matches:IMatchComplete[], leadeboard:ILeaderboard[]):ILeaderboard[] => {
+  checkDrawn = (
+    matches: IMatchComplete[],
+    leadeboard: ILeaderboard[],
+    includAwayInfo = false,
+    includeHomeInfo = false,
+  ): ILeaderboard[] => {
     let newLeaderboard = leadeboard;
     matches.forEach((match) => {
       if (match.awayTeamGoals === match.homeTeamGoals) {
-        newLeaderboard = this.processDraw(newLeaderboard, match.teamHome.teamName);
-        newLeaderboard = this.processDraw(newLeaderboard, match.teamAway.teamName);
+        if (includeHomeInfo) {
+          newLeaderboard = this.processDraw(newLeaderboard, match.teamHome.teamName);
+        }
+        if (includAwayInfo) {
+          newLeaderboard = this.processDraw(newLeaderboard, match.teamAway.teamName);
+        }
       }
     });
     return newLeaderboard;
   };
 
-  checkVictories = (matches:IMatchComplete[], leadeboard:ILeaderboard[]):ILeaderboard[] => {
+  private processHomeWin = (
+    match:IMatchComplete,
+    leadeboard: ILeaderboard[],
+    includAwayInfo:boolean,
+    includeHomeInfo:boolean,
+  ) => {
+    let newLeaderboard = leadeboard;
+    if (includeHomeInfo) {
+      newLeaderboard = this.processVictory(newLeaderboard, match.teamHome.teamName);
+    }
+    if (includAwayInfo) {
+      newLeaderboard = this.processLoss(newLeaderboard, match.teamAway.teamName);
+    }
+    return newLeaderboard;
+  };
+
+  private processAwayWin = (
+    match:IMatchComplete,
+    leadeboard: ILeaderboard[],
+    includAwayInfo:boolean,
+    includeHomeInfo:boolean,
+  ) => {
+    let newLeaderboard = leadeboard;
+    if (includeHomeInfo) {
+      newLeaderboard = this.processLoss(newLeaderboard, match.teamHome.teamName);
+    }
+    if (includAwayInfo) {
+      newLeaderboard = this.processVictory(newLeaderboard, match.teamAway.teamName);
+    }
+    return newLeaderboard;
+  };
+
+  checkVictories = (
+    matches: IMatchComplete[],
+    leadeboard: ILeaderboard[],
+    includAwayInfo = false,
+    includeHomeInfo = false,
+  ): ILeaderboard[] => {
     let newLeaderboard = leadeboard;
     matches.forEach((match) => {
       if (match.homeTeamGoals > match.awayTeamGoals) {
-        newLeaderboard = this.processVictory(newLeaderboard, match.teamHome.teamName);
-        newLeaderboard = this.processLoss(newLeaderboard, match.teamAway.teamName);
+        newLeaderboard = this
+          .processHomeWin(match, newLeaderboard, includAwayInfo, includeHomeInfo);
       }
       if (match.homeTeamGoals < match.awayTeamGoals) {
-        newLeaderboard = this.processLoss(newLeaderboard, match.teamHome.teamName);
-        newLeaderboard = this.processVictory(newLeaderboard, match.teamAway.teamName);
+        newLeaderboard = this
+          .processAwayWin(match, newLeaderboard, includAwayInfo, includeHomeInfo);
       }
     });
     return newLeaderboard;
   };
 
-  countGoals = (matches:IMatchComplete[], leadeboard:ILeaderboard[]):ILeaderboard[] => {
+  // checkVictories = (
+  //   matches: IMatchComplete[],
+  //   leadeboard: ILeaderboard[],
+  //   includAwayInfo = false,
+  //   includeHomeInfo = false,
+  // ): ILeaderboard[] => {
+  //   let newLeaderboard = leadeboard;
+  //   matches.forEach((match) => {
+  //     if (match.homeTeamGoals > match.awayTeamGoals) {
+  //       newLeaderboard = this.processVictory(newLeaderboard, match.teamHome.teamName);
+  //       newLeaderboard = this.processLoss(newLeaderboard, match.teamAway.teamName);
+  //     }
+  //     if (match.homeTeamGoals < match.awayTeamGoals) {
+  //       newLeaderboard = this.processLoss(newLeaderboard, match.teamHome.teamName);
+  //       newLeaderboard = this.processVictory(newLeaderboard, match.teamAway.teamName);
+  //     }
+  //   });
+  //   return newLeaderboard;
+  // };
+
+  countGoals = (
+    matches: IMatchComplete[],
+    leadeboard: ILeaderboard[],
+    includAwayInfo = false,
+    includeHomeInfo = false,
+  ): ILeaderboard[] => {
     let newLeaderboard = leadeboard;
     matches.forEach((match) => {
-      newLeaderboard = this.processGoals(
-        newLeaderboard,
-        match.teamHome.teamName,
-        match.homeTeamGoals,
-        match.awayTeamGoals,
-      );
-      newLeaderboard = this.processGoals(
-        newLeaderboard,
-        match.teamAway.teamName,
-        match.awayTeamGoals,
-        match.homeTeamGoals,
-      );
+      const { homeTeamGoals: homeG, awayTeamGoals: awayG } = match;
+      if (includeHomeInfo) {
+        newLeaderboard = this.processGoals(newLeaderboard, match.teamHome.teamName, homeG, awayG);
+      }
+      if (includAwayInfo) {
+        newLeaderboard = this.processGoals(newLeaderboard, match.teamAway.teamName, awayG, homeG);
+      }
     });
     return newLeaderboard;
   };
 
-  calcGoalBalance = (leaderboard:ILeaderboard[]): ILeaderboard[] => {
+  calcGoalBalance = (leaderboard: ILeaderboard[]): ILeaderboard[] => {
     const newLeaderboard = leaderboard;
     newLeaderboard.forEach((team, index) => {
       newLeaderboard[index].goalsBalance = team.goalsFavor - team.goalsOwn;
@@ -100,7 +181,7 @@ export default class LeaderboardDTService implements ILeaderboardDTS {
     return newLeaderboard;
   };
 
-  calcEfficiency = (leaderboard:ILeaderboard[]): ILeaderboard[] => {
+  calcEfficiency = (leaderboard: ILeaderboard[]): ILeaderboard[] => {
     const newLeaderboard = leaderboard;
     newLeaderboard.forEach((team, index) => {
       newLeaderboard[index]
@@ -109,7 +190,7 @@ export default class LeaderboardDTService implements ILeaderboardDTS {
     return newLeaderboard;
   };
 
-  calcTotalGames = (leaderboard:ILeaderboard[]): ILeaderboard[] => {
+  calcTotalGames = (leaderboard: ILeaderboard[]): ILeaderboard[] => {
     const newLeaderboard = leaderboard;
     newLeaderboard.forEach((team, index) => {
       newLeaderboard[index].totalGames = team.totalDraws + team.totalLosses + team.totalVictories;
@@ -117,7 +198,7 @@ export default class LeaderboardDTService implements ILeaderboardDTS {
     return newLeaderboard;
   };
 
-  calcTotalPoints = (leaderboard:ILeaderboard[]): ILeaderboard[] => {
+  calcTotalPoints = (leaderboard: ILeaderboard[]): ILeaderboard[] => {
     const newLeaderboard = leaderboard;
     newLeaderboard.forEach((team, index) => {
       newLeaderboard[index].totalPoints = team.totalDraws + 3 * team.totalVictories;
@@ -125,26 +206,32 @@ export default class LeaderboardDTService implements ILeaderboardDTS {
     return newLeaderboard;
   };
 
-  private sortGoalsBalance = (a:ILeaderboard, b:ILeaderboard):number => {
-    if (b.goalsBalance === a.goalsBalance) {
-      if (b.goalsFavor === a.goalsFavor) return a.goalsOwn - b.goalsOwn;
-      return b.goalsFavor - a.goalsFavor;
-    }
-    if (b.goalsBalance > a.goalsBalance) return 1;
-    return -1;
+  private sortGoalsOwn = (
+    a: ILeaderboard,
+    b: ILeaderboard,
+  ): number => (b.goalsOwn > a.goalsOwn ? 1 : -1);
+
+  private sortGoalsFavor = (a: ILeaderboard, b: ILeaderboard): number => {
+    if (b.goalsFavor === a.goalsFavor) return this.sortGoalsOwn(a, b);
+    return b.goalsFavor - a.goalsFavor;
+  };
+
+  private sortGoalsBalance = (a: ILeaderboard, b: ILeaderboard): number => {
+    if (b.goalsBalance === a.goalsBalance) return this.sortGoalsFavor(a, b);
+    return b.goalsBalance > a.goalsBalance ? 1 : -1;
+  };
+
+  private sortVictories = (a: ILeaderboard, b: ILeaderboard): number => {
+    if (b.totalVictories === a.totalVictories) return this.sortGoalsBalance(a, b);
+    return b.totalVictories - a.totalVictories;
   };
 
   // Ordem para desempate
   // 1º Total de Vitórias; 2º Saldo de gols; 3º Gols a favor; 4º Gols sofridos.
-  sortClassification = (leaderboard:ILeaderboard[]): ILeaderboard[] => {
+  sortClassification = (leaderboard: ILeaderboard[]): ILeaderboard[] => {
     const newLeaderboard = leaderboard;
     newLeaderboard.sort((a, b) => {
-      if (b.totalPoints === a.totalPoints) {
-        if (b.totalVictories === a.totalVictories) {
-          return this.sortGoalsBalance(a, b);
-        }
-        return b.totalVictories - a.totalVictories;
-      }
+      if (b.totalPoints === a.totalPoints) return this.sortVictories(a, b);
       return b.totalPoints - a.totalPoints;
     });
     return newLeaderboard;
