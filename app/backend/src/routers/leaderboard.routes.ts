f9af1e6307f88474a0b558d786f0af37df79e9b9
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-24 16:01:47 -0300

    feat: :sparkles: Add get /leaderboard route

--- app/backend/src/routers/leaderboard.routes.ts
+++ app/backend/src/routers/leaderboard.routes.ts
@@ -2,16 +2,26 @@ import { Router } from 'express';
 import MatchesService from '../services/databaseInteraction/matches.service';
 import LeaderboardController from '../controllers/leaderboard.controller';
 import LeaderboardService from '../services/leaderboard/leaderboard.service';
+import { TeamsService } from '../services/databaseInteraction/teams.service';
+import LeaderboardDTService from '../services/leaderboard/dataTreatement.service';
 
 const router = Router();
 
 const matchesService = new MatchesService();
-const leaderboardService = new LeaderboardService();
-const leaderboardController = new LeaderboardController(matchesService, leaderboardService);
+const leaderboardDTService = new LeaderboardDTService();
+const leaderboardService = new LeaderboardService(leaderboardDTService);
+const teamsService = new TeamsService();
+const leaderboardController = new
+LeaderboardController(matchesService, leaderboardService, teamsService);
 
 router.get(
   '/leaderboard/home',
   leaderboardController.getLeaderboardHome,
+);
+
+router.get(
+  '/leaderboard',
+  leaderboardController.getLeaderboard,
 );
 
 export default router;
