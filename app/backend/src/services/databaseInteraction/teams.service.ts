90a686aabce6ae14bd8edf6bb635f1634430aaa2
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-25 17:52:59 -0300

    feat: Create method FindTeamsById.

--- app/backend/src/services/databaseInteraction/teams.service.ts
+++ app/backend/src/services/databaseInteraction/teams.service.ts
@@ -3,6 +3,7 @@ import Teams from '../../database/models/teams';
 export interface ITeamsService {
   list(): Promise<Teams[]>,
   findById(id:number): Promise<Teams | null>,
+  findTeamsById(array:number[]): Promise<Teams[]>,
 }
 
 export class TeamsService implements ITeamsService {
@@ -14,5 +15,10 @@ export class TeamsService implements ITeamsService {
   findById = async (id:number): Promise<Teams | null> => {
     const team: Teams | null = await Teams.findByPk(id);
     return team;
+  };
+
+  findTeamsById = async (array:number[]): Promise<Teams[]> => {
+    const teams: Teams[] = await Teams.findAll({ where: { id: array } });
+    return teams;
   };
 }
