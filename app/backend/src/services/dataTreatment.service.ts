e185b3937b24015297808c7dcf8461c564b7487d
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-25 18:37:44 -0300

    chore: :fire: remove realocated file.

--- app/backend/src/services/dataTreatment.service.ts
+++
@@ -1,7 +0,0 @@
-// export interface IDataTreatment {
-//   returnLeaderBoardHome(): Promise<void>,
-//   returnLeaderBoardAway(): Promise<void>,
-//   returnLeaderBoard(): Promise<void>,
-// }
-
-// export default class DataTratmentService implements IDataTreatment {}
