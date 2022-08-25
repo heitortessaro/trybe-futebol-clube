190fb4be1529a941892dbb9797504c3ec1ecd48c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-08-25 15:05:27 -0300

    test: :white_check_mark: Extends teams tests and create mock file.
    Create a mock file with teams information. Add a test to verify the behavior when there is no team with the provided id.

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -8,34 +8,39 @@ import { Response } from 'superagent';
 import { ITeam } from '../interfaces/ITeam';
 import { StatusCodes } from 'http-status-codes';
 import Teams from '../database/models/teams';
+// const teams = require('./mocks/teams/teams')
+import teams from './mocks/teams/teams'
+
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 //  Service Response Mock
-const teamsMock: ITeam[] = [
-  {
-    "id": 1,
-    "teamName": "Avaí/Kindermann"
-  },
-  {
-    "id": 2,
-    "teamName": "Bahia"
-  },
-  {
-    "id": 3,
-    "teamName": "Botafogo"
-  },
-];
+const teamsMock: ITeam[] = teams;
+const teamMock: ITeam = teams[1];
+// [
+//   {
+//     "id": 1,
+//     "teamName": "Avaí/Kindermann"
+//   },
+//   {
+//     "id": 2,
+//     "teamName": "Bahia"
+//   },
+//   {
+//     "id": 3,
+//     "teamName": "Botafogo"
+//   },
+// ];
 
-const teamMock: ITeam = {
-  "id": 1,
-  "teamName": "Avaí/Kindermann"
-}
+// const teamMock: ITeam = {
+//   "id": 1,
+//   "teamName": "Avaí/Kindermann"
+// }
 
 describe('Teams', () => {
-  describe('Get Teams', () => {
+  describe('Get All Teams', () => {
     afterEach(() => {
       sinon.restore();
     })
@@ -52,4 +57,22 @@ describe('Teams', () => {
       expect(response.body).to.deep.equal(teamMock);
     });
   });
+  describe('Get Teams by Id', () => {
+    afterEach(() => {
+      sinon.restore();
+    })
+    it('Should return an object with the related team information status code 200', async () => {
+      sinon.stub(Teams, 'findOne').resolves(teamMock as Teams)
+      const response = await chai.request(app).get('/teams/1');
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.body).to.deep.equal(teamMock);
+    });
+    it('Should return an error message when team id does not exist', async () => {
+      sinon.stub(Teams, 'findOne').resolves(null)
+      const response = await chai.request(app).get('/teams/100');
+      expect(response.status).to.equal(StatusCodes.NOT_FOUND);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('There is no team with such id!')
+    })
+  })
 });
